d60e057812716cc60417aee513ad176cf0e1149e
williamMDsilva <william.moreirasilva@hotmail.com>
2022-05-27 16:40:41 +0000

    Deploying to gh-pages from  @ e3597a1de59cb11125c1fb9008a84cd2b691fd5a 🚀

---
+++ _next/static/_7Jba7RBXznhS4YNlCphI/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/726-e21aa8b4675aef3f.js","static/chunks/pages/index-6eea38c7989bde1d.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
