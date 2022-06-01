4029b84e5621fe012b622b0d08725071ff984977
williamMDsilva <william.moreirasilva@hotmail.com>
2022-06-01 23:29:21 +0000

    Deploying to gh-pages from  @ 3f806e833ebf5d6c5ade0908a70c138fc30c8b50 🚀

--- _next/static/jbH3f0rV3LhFYxW65PoPZ/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/3-f28ef5b0f78f9141.js","static/chunks/pages/index-f7c9ca53ce9da16d.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
